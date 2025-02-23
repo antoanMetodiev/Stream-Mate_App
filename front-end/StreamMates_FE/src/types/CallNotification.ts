e88a6db081f9799b665b5450410b225820e462a6
Antoan Metodiev <Boksnem@abv.bg>
2025-02-24 00:54:04 +0200

    add type for call notification

---
+++ front-end/StreamMates_FE/src/types/CallNotification.ts
@@ -0,0 +1,10 @@
+import { CallType } from "./enums/CallType";
+
+export type CallNotification = {
+    caller: string;
+    receiver: string;
+    callId: string; // Уникален идентификатор на обаждането
+    callType: CallType; // "video" или "audio"
+    channelName: string; // WebRTC / Agora канал
+    timestamp: string; // Време на обаждането (ISO формат)
+};
