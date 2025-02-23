9f82550456a334826ce3082f643cdec91cbb09ec
Antoan Metodiev <Boksnem@abv.bg>
2025-02-24 01:03:02 +0200

    create UserRole Type

---
+++ front-end/StreamMates_FE/src/types/enums/UserRole.ts
@@ -0,0 +1,7 @@
+import { User } from "../User";
+
+export type UserImage = {
+    id: string; // UUID ще се мапва към string
+    image_url: string; // Съответства на image_url в Java
+    owner: User; // Референция към User типа
+};
