9e2aa68f6201fc538db6451bbb0488eeaed2601d
Antoan Metodiev <Boksnem@abv.bg>
2025-02-23 21:57:07 +0200

    add Actor Component

---
+++ front-end/StreamMates_FE/src/components/Movies/CinemaRecordsList/CinemaRecord/CinemaRecordDetails/CastSection/Actor/Actor.tsx
@@ -0,0 +1,32 @@
+import { constants } from "../../../../../../../constants/constants";
+import { ActorType } from "../../../../../../../types/ActorType";
+import style from "./Actor.module.css";
+
+interface ActorProps {
+    actor: ActorType | undefined;
+}
+
+export const Actor = ({
+    actor
+}: ActorProps) => {
+
+
+
+    return (
+        <div className={style['actor-container']}>
+
+
+            <div>
+                <img
+                    className={style['actor-image']}
+                    src={actor?.imageURL?.length == 0 ? constants.Actor_BASE_IMG_URL
+                        : constants.TMDB_IMG_URL + actor?.imageURL}
+                    alt={actor?.nameInRealLife}
+                />
+                <span className={style['shadow']}></span>
+            </div>
+            <h4 className={style['actor-real-name']}>{actor?.nameInRealLife}</h4>
+            {/* <h6 className={style['actor-name-in-show']}>{actor.}</h6> */}
+        </div>
+    );
+};
