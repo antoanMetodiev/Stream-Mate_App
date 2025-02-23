e41cf9827985e51654dbd89bcae38988bed159ce
Antoan Metodiev <Boksnem@abv.bg>
2025-02-24 00:52:22 +0200

    add component who contain searched or only for the first page - movies or series

---
+++ front-end/StreamMates_FE/src/components/Movies/CinemaRecordsList/CinemaRecordsList.tsx
@@ -0,0 +1,28 @@
+import { Movie } from "../../../types/MovieType";
+import { Series } from "../../../types/Series";
+import { CinemaRecord } from "./CinemaRecord/CinemaRecord";
+import style from "./CinemaRecordsList.module.css";
+
+interface CinemaRecordsListProps {
+    cinemaRecordsList: (Movie | Series)[] | undefined;
+}
+
+export const CinemaRecordsList = ({
+    cinemaRecordsList
+}: CinemaRecordsListProps) => {
+
+
+    
+
+    return (
+        <section className={style["cinema-record-list-container"]}>
+            {cinemaRecordsList?.map((cinemaRecord) => (
+                <CinemaRecord
+                    key={cinemaRecord.id}
+                    cinemaRecord={cinemaRecord}
+                    cinemaRecordsList={cinemaRecordsList}
+                />
+            ))}
+        </section>
+    );
+};
