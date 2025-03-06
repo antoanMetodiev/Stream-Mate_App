a2486cce4ebaea4251e54291239a1df07a833a21
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-07 01:06:53 +0200

    add user image entity

--- back-end/src/main/java/bg/stream_mates/backend/feather/user/models/entities/UserImage.java
+++ back-end/src/main/java/bg/stream_mates/backend/feather/user/models/entities/UserImage.java
@@ -1,7 +1,12 @@
 package bg.stream_mates.backend.feather.user.models.entities;
 
+import bg.stream_mates.backend.feather.user.models.enums.UserImageType;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import org.hibernate.validator.constraints.URL;
 
@@ -11,6 +16,9 @@ import java.util.UUID;
 @Table(name = "user_images")
 @Data
 @Accessors(chain = true)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserImage {
 
     @Id
@@ -21,6 +29,14 @@ public class UserImage {
     @URL
     private String image_url;
 
+    @Column
+    @Enumerated(EnumType.STRING)
+    private UserImageType userImageType;
+
+    @Column
+    private String description;
+
     @ManyToOne
+    @JsonBackReference
     private User owner;
 }
