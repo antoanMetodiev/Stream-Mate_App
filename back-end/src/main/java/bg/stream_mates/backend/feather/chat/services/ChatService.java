db9a500cebcf400384c4991ad6747d588667a746
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-07 00:57:44 +0200

    add logics about send/receive 3 different type messages

--- back-end/src/main/java/bg/stream_mates/backend/feather/chat/services/ChatService.java
+++ back-end/src/main/java/bg/stream_mates/backend/feather/chat/services/ChatService.java
@@ -3,23 +3,31 @@ package bg.stream_mates.backend.feather.chat.services;
 import bg.stream_mates.backend.feather.chat.models.dtos.CallNotification;
 import bg.stream_mates.backend.feather.chat.models.dtos.ReceivedMessage;
 import bg.stream_mates.backend.feather.chat.models.entities.Message;
+import bg.stream_mates.backend.feather.chat.models.enums.CallType;
+import bg.stream_mates.backend.feather.chat.models.enums.MessageType;
 import bg.stream_mates.backend.feather.chat.repositories.ChatRepository;
 import bg.stream_mates.backend.feather.user.models.entities.User;
 import bg.stream_mates.backend.feather.user.repositories.UserRepository;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.time.Instant;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class ChatService extends TextWebSocketHandler {
+    // Map за съхранение на активните потребители (userId -> WebSocketSession)
+    private static final Map<String, WebSocketSession> activeSessions = new ConcurrentHashMap<>();
 
-    private final ChatWebSocketHandler chatWebSocketHandler;
     private final ObjectMapper objectMapper;
     private final ChatRepository chatRepository;
     private final UserRepository userRepository;
@@ -27,36 +35,72 @@ public class ChatService extends TextWebSocketHandler {
     @Autowired
     public ChatService(ObjectMapper objectMapper,
                        ChatRepository chatRepository,
-                       UserRepository userRepository,
-                       ChatWebSocketHandler chatWebSocketHandler) {
+                       UserRepository userRepository) {
 
         this.objectMapper = objectMapper;
         this.chatRepository = chatRepository;
         this.userRepository = userRepository;
-        this.chatWebSocketHandler = chatWebSocketHandler;
     }
 
-    // FOR VIDEO CALLS OR AUDIO;
-    @KafkaListener(topics = "call-topic", groupId = "video-call-group")
-    public void handleCallFromKafka(String messageJson) throws Exception {
-        CallNotification videoCall = objectMapper.readValue(messageJson, CallNotification.class);
-
-        System.out.println("Received video call request from Kafka - From: " + videoCall.getCaller() +
-                ", To: " + videoCall.getReceiver());
-
-        // Изпращане на уведомление чрез WebSocket
-        chatWebSocketHandler.sendVideoCallToUser(videoCall);
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        String userId = session.getUri().getQuery().replace("username=", "");
+        activeSessions.put(userId, session);
+        System.out.println("User connected: " + userId + " | Session ID: " + session.getId());
     }
 
-    // Това ми се явява моя Kafka Consumer:
-    @KafkaListener(topics = "chat-topic", groupId = "chat-group")
-    public void handleMessageFromKafka(String messageJson) throws Exception {
-        ReceivedMessage receivedMessage = objectMapper.readValue(messageJson, ReceivedMessage.class);
-        System.out.println("Received from Kafka - To: " + receivedMessage.getReceiver() +
-                ", Message: " + receivedMessage.getMessageText());
+    // ПОЛУЧАВА С WebSocket:
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        // Опитваме се да разпознаем типа на съобщението (текстово или видео обаждане)
+        JsonNode jsonNode = objectMapper.readTree(message.getPayload());
 
-        // Изпращане на съобщението към Back-End WebSocket (съседния клас), за да може той да го изпрати на FE:
-        chatWebSocketHandler.sendMessageToUser(receivedMessage);
+        if (jsonNode.has("callId")) {
+            // Това е за видео обаждане:
+            CallNotification callNotification = objectMapper.treeToValue(jsonNode, CallNotification.class);
+            sendCallToUser(callNotification);
+            System.out.println("Video/Audio call sent to Kafka: " + callNotification);
+        } else {
+            // Това е стандартно текстово съобщение
+            ReceivedMessage receivedMessage = objectMapper.treeToValue(jsonNode, ReceivedMessage.class);
+            sendMessageToUser(receivedMessage);
+            this.saveMessageToDB(receivedMessage);
+            System.out.println("Message sent to Kafka: " + receivedMessage);
+        }
+    }
+
+    // ИЗПРАЩА С WebSocket:
+    public void sendMessageToUser(ReceivedMessage message) throws Exception {
+        WebSocketSession recipientSession = activeSessions.get(message.getReceiver());
+        if (recipientSession != null && recipientSession.isOpen()) {
+            recipientSession.sendMessage(new TextMessage(message.getMessageText()));
+            System.out.println("Sent message to " + message.getReceiver() + ": " + message.getMessageText());
+        } else {
+            System.out.println("User " + message.getReceiver() + " not found or not connected.");
+        }
+    }
+
+    // VIDEO CALLS:
+    public void sendCallToUser(CallNotification message) throws Exception {
+        String jsonMessage = objectMapper.writeValueAsString(message);
+        WebSocketSession recipientSession = activeSessions.get(message.getReceiver());
+        if (recipientSession != null && recipientSession.isOpen()) {
+            // Изпращаме съобщението като TextMessage:
+            recipientSession.sendMessage(new TextMessage(jsonMessage));
+            System.out.println("Sent video call notification to " + message.getReceiver());
+        } else {
+            System.out.println("User " + message.getReceiver() + " not found or not connected.");
+        }
+
+        // Винаги съхранваме, това което е искал да изпрати real-time, дори човека отсреща да не е на линия:
+        ReceivedMessage receivedMessage = ReceivedMessage.builder()
+                .messageType(MessageType.AUDIO_CALL)
+                .owner(message.getCaller())
+                .receiver(message.getReceiver())
+                .messageText(" :} ")
+                .build();
+
+        if (message.getCallType() == CallType.VIDEO_CALL) receivedMessage.setMessageType(MessageType.VIDEO_CALL);
         this.saveMessageToDB(receivedMessage);
     }
 
@@ -67,12 +111,29 @@ public class ChatService extends TextWebSocketHandler {
         if (owner.isEmpty() || receiver.isEmpty()) return;
 
         Message message = Message.builder()
-                .messageText(receivedMessage.getMessageText())
                 .owner(owner.get())
                 .receiver(receiver.get())
                 .createdOn(Instant.now())
+                .messageType(receivedMessage.getMessageType())
                 .build();
 
+        if (receivedMessage.getMessageType() == MessageType.IMAGE
+                || receivedMessage.getMessageType() == MessageType.TEXT) {
+            message.setMessageText(receivedMessage.getMessageText());
+        } else {
+            message.setMessageText(String.format("%s започна аудио обаждане.", message.getOwner()));
+            if (message.getMessageType() == MessageType.VIDEO_CALL) {
+                message.setMessageText(String.format("%s започна аудио обаждане.", message.getOwner()));
+            }
+        }
+
         this.chatRepository.save(message);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        // Премахване на сесията при затваряне
+        activeSessions.values().remove(session);
+        System.out.println("Chat Session closed: " + session.getId());
     }
 }
