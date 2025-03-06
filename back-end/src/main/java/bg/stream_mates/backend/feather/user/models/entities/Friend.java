91216fa09662687960a44d58783700e79ca1c45f
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-07 01:04:14 +0200

    add entity who contain most importan info for userFriend (not all User info in my friends list)

--- back-end/src/main/java/bg/stream_mates/backend/feather/user/models/entities/Friend.java
+++ back-end/src/main/java/bg/stream_mates/backend/feather/user/models/entities/Friend.java
@@ -1,7 +1,10 @@
 package bg.stream_mates.backend.feather.user.models.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import org.hibernate.validator.constraints.URL;
 
@@ -10,12 +13,18 @@ import java.util.UUID;
 @Entity
 @Table(name = "friends")
 @Data
+@Builder
 @Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class Friend {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
 
     @Column(name = "first_name", nullable = false)
     private String firstName;
