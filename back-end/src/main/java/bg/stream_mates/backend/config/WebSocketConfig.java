2eb2f8909f0ac7106814f77a01b320a442605621
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-07 00:58:22 +0200

    remove unuseable beans

--- back-end/src/main/java/bg/stream_mates/backend/config/WebSocketConfig.java
+++ back-end/src/main/java/bg/stream_mates/backend/config/WebSocketConfig.java
@@ -1,10 +1,12 @@
 package bg.stream_mates.backend.config;
 
-import bg.stream_mates.backend.feather.chat.services.ChatWebSocketHandler;
-import bg.stream_mates.backend.feather.chat.models.dtos.ReceivedMessage;
+import bg.stream_mates.backend.feather.chat.repositories.ChatRepository;
+import bg.stream_mates.backend.feather.chat.services.ChatService;
+import bg.stream_mates.backend.feather.user.handlers.FriendRequestNotificationHandler;
+import bg.stream_mates.backend.feather.user.repositories.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -13,19 +15,36 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+    private final ChatRepository chatRepository;
+    private final UserRepository userRepository;
+    private final FriendRequestNotificationHandler friendRequestNotificationHandler;
 
-    public WebSocketConfig(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
+    public WebSocketConfig(ObjectMapper objectMapper,
+                           ChatRepository chatRepository,
+                           UserRepository userRepository, FriendRequestNotificationHandler friendRequestNotificationHandler) {
+
+        this.objectMapper = objectMapper;
+        this.chatRepository = chatRepository;
+        this.userRepository = userRepository;
+        this.friendRequestNotificationHandler = friendRequestNotificationHandler;
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(chatWebSocketHandler(), "/chat").setAllowedOrigins("*");
+        registry.addHandler(friendRequestNotificationHandler, "/frRequest").setAllowedOrigins("*");
     }
 
-    @Bean
-    public ChatWebSocketHandler chatWebSocketHandler() {
-        return new ChatWebSocketHandler(kafkaTemplate);
+    @Bean(name = "customChatWebSocketHandler")
+    public ChatService chatWebSocketHandler() {
+        return new ChatService(objectMapper, chatRepository, userRepository);
     }
+
+    // !!! ТОВА ОТДОЛУ ГО МАХНАХ, ЗАЩОТО РЕГИСТРИРАХ ТОЗИ БЕАН 2-ПЪТИ - НЕ ЗНАМ ДАЛИ СЪС ChatService СТАВА СЪЩОТО И ДАЛИ НЕ ТРЯБВА ДА ГО МАХНА И НЕГО ТОЗИ ГОРНИЯ МЕТОД
+
+//    @Bean(name = "customFriendRequestNotificationHandler")
+//    public FriendRequestNotificationHandler friendRequestNotificationHandler() {
+//        return new FriendRequestNotificationHandler(objectMapper);
+//    }
 }
