d711b9d387c42dca62e03ab5cff330feb30adfc4
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-07 00:54:58 +0200

    create entity for received message like (TextMessage, AudioCall or VideoCall))

--- back-end/src/main/java/bg/stream_mates/backend/feather/chat/models/dtos/ReceivedMessage.java
+++ back-end/src/main/java/bg/stream_mates/backend/feather/chat/models/dtos/ReceivedMessage.java
@@ -1,9 +1,15 @@
 package bg.stream_mates.backend.feather.chat.models.dtos;
 
+import bg.stream_mates.backend.feather.chat.models.enums.MessageType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ReceivedMessage {
 
     @NotBlank(message = "Message is empty!")
@@ -14,4 +20,8 @@ public class ReceivedMessage {
 
     @NotBlank(message = "Receiver is empty!")
     private String receiver;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private MessageType messageType;
 }
