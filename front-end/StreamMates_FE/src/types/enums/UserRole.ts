e10c17f6fe64a6d31e23df44cd361a8304398228
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-07 16:17:53 +0200

    add description prop

--- front-end/StreamMates_FE/src/types/enums/UserRole.ts
+++ front-end/StreamMates_FE/src/types/enums/UserRole.ts
@@ -4,4 +4,5 @@ export type UserImage = {
     id: string; // UUID ще се мапва към string
     image_url: string; // Съответства на image_url в Java
     owner: User; // Референция към User типа
+    description: string,
 };
