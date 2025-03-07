c0ae47fc28ca6fbfbb16cf948cbc5d85abfd7f3f
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-07 16:31:23 +0200

    add condition

--- front-end/StreamMates_FE/src/components/Movies/CinemaRecordsList/CinemaRecord/CinemaRecordDetails/PlayerSection/PlayerSection.tsx
+++ front-end/StreamMates_FE/src/components/Movies/CinemaRecordsList/CinemaRecord/CinemaRecordDetails/PlayerSection/PlayerSection.tsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import style from "./PlayerSection.module.css";
 
 interface PlayerSectionProps {
-    videoURL: string
+    videoURL: string | null | undefined
 };
 
 export const PlayerSection = ({
@@ -14,13 +14,15 @@ export const PlayerSection = ({
 
     return (
         <section className={style['player-section-container']}>
-            <iframe
-                ref={iframeRef}
-                src={videoURL}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="Video"
-            ></iframe>
+            {videoURL && (
+                <iframe
+                    ref={iframeRef}
+                    src={videoURL}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    title="Video"
+                ></iframe>
+            )}
         </section>
     );
 };
