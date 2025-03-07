2846db69702c05fca599451e7f65ca3c22c724e7
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-07 15:50:45 +0200

    add link and dynamic url variable

--- front-end/StreamMates_FE/src/components/HomePage/WelcomeComponent/Navigation/Navigation.tsx
+++ front-end/StreamMates_FE/src/components/HomePage/WelcomeComponent/Navigation/Navigation.tsx
@@ -13,9 +13,13 @@ export const Navigation = ({
     user,
     setUser,
 }: NavigationProps) => {
+
+
     const logout = async () => {
+        const BASE_URL = window.location.href.includes("local") ? "http://localhost:8080" : "https://streammate-org.onrender.com";
+        
         try {
-            const response = await axios.delete("http://localhost:8080/logout", {
+            const response = await axios.delete(BASE_URL + "/logout", {
                 withCredentials: true,
                 headers: {
                     "X-Custom-Logout": "true" // 👈 Case-sensitive!
@@ -28,13 +32,14 @@ export const Navigation = ({
         };
     };
 
+
     return (
         <nav className={style['navigation-container']}>
             {!user && <Link to="/login" className={style['link']}>Login</Link>}
             {!user && <Link to="/register" className={style['link']}>Register</Link>}
             {user && <Link to="/movies" className={style['link']}>Movies</Link>}
             {user && <Link to="/series" className={style['link']}>Series</Link>} 
-            {user && <Link to="" className={style['link']}>24/7 Channels</Link>}
+            {user && <Link to="/tv-channels" className={style['link']}>24/7 Channels</Link>}
             {user && <button onClick={logout} className={style['link']}>Logout</button> }
         </nav>
     );
