b5df844167fcd84360cbaf8bbaf93e16206caff5
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-07 15:42:23 +0200

    remove @CrossOrigin anotations

--- back-end/src/main/java/bg/stream_mates/backend/feather/series/controllers/SeriesController.java
+++ back-end/src/main/java/bg/stream_mates/backend/feather/series/controllers/SeriesController.java
@@ -3,7 +3,6 @@ package bg.stream_mates.backend.feather.series.controllers;
 import bg.stream_mates.backend.feather.commonData.dtos.CinemaRecRequestDto;
 import bg.stream_mates.backend.feather.series.models.Series;
 import bg.stream_mates.backend.feather.series.services.SeriesService;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,13 +18,11 @@ public class SeriesController {
         this.seriesService = cinemaRecService;
     }
 
-    @CrossOrigin(origins = "http://localhost:5173") // Позволява заявки от localhost:5173
     @PostMapping("/search-series")
     public void searchSeries(@RequestBody CinemaRecRequestDto cinemaRecRequestDto) throws IOException, InterruptedException {
         this.seriesService.searchForSeries(cinemaRecRequestDto.getRecordName());
     }
 
-    @CrossOrigin(origins = "http://localhost:5173") // Позволява заявки от localhost:5173
     @PostMapping("/get-series")
     public List<Series> getSeries(@RequestBody CinemaRecRequestDto cinemaRecRequestDto) throws IOException, InterruptedException {
         return this.seriesService.getSeries(cinemaRecRequestDto.getRecordName());
