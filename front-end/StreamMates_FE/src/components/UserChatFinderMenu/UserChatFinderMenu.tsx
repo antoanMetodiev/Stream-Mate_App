ee4b428a2303362aff269f3ae6a1e1c302441637
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-11 15:56:18 +0200

    add chat section in component

--- front-end/StreamMates_FE/src/components/UserChatFinderMenu/UserChatFinderMenu.tsx
+++ front-end/StreamMates_FE/src/components/UserChatFinderMenu/UserChatFinderMenu.tsx
@@ -1,29 +1,55 @@
 import style from "./UserChatFinderMenu.module.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import logoIcon from "./images/user.png";
 import chatIcon from "./images/chat.png";
 import usersIcon from "./images/users.png";
 import receivedFriendRequestsIcon from "./images/received-friend-requests.png";
 import friendsIcon from "./images/friends.png";
+import rejectCallImg from "./images/leave-channel.png";
+import acceptCallImg from "./images/audio-call.png";
+import deffaultUserImg from "./images/deffault-user-image.jpg";
 
 import { FindUsers } from "./FindUsers/FindUsers";
 import { User } from "../../types/User";
 import { ReceivedFriendRequests } from "./ReceivedFriendRequests/ReceivedFriendRequests";
 import { FriendsSection } from "./FriendsSection/FriendsSection";
+import { CallNotification } from "../../types/CallNotification";
+import { ChatSection } from "./ChatSection/ChatSection";
+import { Message } from "../../types/Message";
+import { Friend } from "../../types/Friend";
+import { VideoCall } from "./ChatSection/Chat/VideoCall/VideoCall";
 
 interface UserChatFinderMenuProps {
-    user: User;
-    setUser: React.Dispatch<React.SetStateAction<User>> | null;
+    user: User | null;
+    setUser: React.Dispatch<React.SetStateAction<User | null>>;
+    webSocket: WebSocket | null;
+    incomingCall: CallNotification | null;
+
+    currentChatFriend: Friend | null;
+    setCurrentChatFriend: React.Dispatch<React.SetStateAction<Friend | null>>;
+    messagesWithCurrentFriend: Message[] | null;
+    setMessagesWithCurrentFriend: React.Dispatch<React.SetStateAction<Message[] | []>>;
+
+    setIncomingCall: React.Dispatch<React.SetStateAction<CallNotification | null>>;
 };
 
 export const UserChatFinderMenu = ({
     user,
     setUser,
+    webSocket,
+    incomingCall,
+    setIncomingCall,
+    currentChatFriend,
+    setCurrentChatFriend,
+    messagesWithCurrentFriend,
+    setMessagesWithCurrentFriend,
 }: UserChatFinderMenuProps) => {
     if (!user) return;
 
+    const [openCallSection, setOpenCallSection] = useState(false);
     const [showMenu, setShowMenu] = useState(false);
+
 
     // Show Sections:
     const [showFindUsersSection, setShowFindUsersSection] = useState(false);
@@ -56,6 +82,27 @@ export const UserChatFinderMenu = ({
 
     return (
         <section className={style['user-chat-finder-menu-container']}>
+            {incomingCall && incomingCall.caller !== user?.id && (
+                <div className={style['incoming-call-container-wrapper']}>
+                    <div className={style['caller-container-wrapper']}>
+                        <img src={incomingCall.callerImgUrl ? incomingCall.callerImgUrl : deffaultUserImg} alt="callerImgUrl" />
+                        <h3 className={style['incoming-call-h3-notification']}>{incomingCall.callerNames}</h3>
+                    </div>
+                    <div className={style['accept-reject-container-imgs-wrapper']}>
+                        <img onClick={() => { setOpenCallSection(true); }} src={acceptCallImg} alt="acceptCallImg" />
+                        <img src={rejectCallImg} alt="rejectCallImg" />
+                    </div>
+                </div>
+            )}
+
+            {openCallSection && (
+                <VideoCall
+                    incomingCall={incomingCall}
+                    setIncomingCall={setIncomingCall}
+                    setOpenCallSection={setOpenCallSection}
+                />
+            )}
+
             {showRecFriendRequestsSection && (
                 <ReceivedFriendRequests
                     user={user}
@@ -64,6 +111,22 @@ export const UserChatFinderMenu = ({
             )}
             {showFindUsersSection && <FindUsers user={user} />}
             {showFriendSection && <FriendsSection user={user} />}
+            {showChatSection && (
+                <ChatSection
+                    user={user}
+                    webSocket={webSocket}
+
+                    currentChatFriend={currentChatFriend}
+                    setCurrentChatFriend={setCurrentChatFriend}
+                    messagesWithCurrentFriend={messagesWithCurrentFriend}
+                    setMessagesWithCurrentFriend={setMessagesWithCurrentFriend}
+                    openCallSection={openCallSection}
+                    setOpenCallSection={setOpenCallSection}
+
+                    incomingCall={incomingCall}
+                    setIncomingCall={setIncomingCall}
+                />
+            )}
 
             {showMenu && (
                 <section className={style['menu-container']}>
