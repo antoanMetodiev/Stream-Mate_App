79e406beb8e2b245b3af0d9cc423317ea67bbfd6
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-11 16:06:49 +0200

    change movie and series links to be buttons

--- front-end/StreamMates_FE/src/components/HomePage/WelcomeComponent/Navigation/Navigation.tsx
+++ front-end/StreamMates_FE/src/components/HomePage/WelcomeComponent/Navigation/Navigation.tsx
@@ -1,19 +1,23 @@
 import axios from "axios";
 import style from "./Navigation.module.css";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { User } from "../../../../types/User";
+import { Movie } from "../../../../types/MovieType";
+import { Series } from "../../../../types/Series";
 
 interface NavigationProps {
     user: User | null;
     setUser: React.Dispatch<React.SetStateAction<User | null>>;
+    setCinemaRecordsList: React.Dispatch<React.SetStateAction<Movie[] | Series[] | undefined>>;
 };
 
 export const Navigation = ({
     user,
     setUser,
+    setCinemaRecordsList,
 }: NavigationProps) => {
-
+    const navigate = useNavigate();
 
     const logout = async () => {
         const BASE_URL = window.location.href.includes("local") ? "http://localhost:8080" : "https://streammate-org.onrender.com";
@@ -22,7 +26,7 @@ export const Navigation = ({
             const response = await axios.delete(BASE_URL + "/logout", {
                 withCredentials: true,
                 headers: {
-                    "X-Custom-Logout": "true" // 👈 Case-sensitive!
+                    "X-Custom-Logout": "true",
                 }
             });
             console.log("Logout successful:", response.data);
@@ -32,15 +36,23 @@ export const Navigation = ({
         };
     };
 
+    const specialNavigateToCinemaRecord = (location: string) => {
+        debugger;
+        localStorage.removeItem("LAST_CINEMA_RECORDS");
+        localStorage.removeItem("LAST_CURRENT_PAGE");
+        if (setCinemaRecordsList) setCinemaRecordsList(undefined);
+        
+        navigate(location);
+    };
 
     return (
         <nav className={style['navigation-container']}>
             {!user && <Link to="/login" className={style['link']}>Login</Link>}
             {!user && <Link to="/register" className={style['link']}>Register</Link>}
-            {user && <Link to="/movies" className={style['link']}>Movies</Link>}
-            {user && <Link to="/series" className={style['link']}>Series</Link>} 
+            {user && <h4 onClick={() => specialNavigateToCinemaRecord("/movies")} className={style['link']}>Movies</h4>}
+            {user && <h4 onClick={() => specialNavigateToCinemaRecord("/series")} className={style['link']}>Series</h4>} 
             {user && <Link to="/tv-channels" className={style['link']}>24/7 Channels</Link>}
-            {user && <button onClick={logout} className={style['link']}>Logout</button> }
+            {user && <h4 onClick={logout} className={style['link']}>Logout</h4> }
         </nav>
     );
 }
