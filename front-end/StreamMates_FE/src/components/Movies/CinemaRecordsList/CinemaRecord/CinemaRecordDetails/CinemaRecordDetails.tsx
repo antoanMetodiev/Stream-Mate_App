d95f5ab6e4b985b5f485d5f5d5158cada3f8f18a
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-11 16:06:19 +0200

    add setur and css

--- front-end/StreamMates_FE/src/components/Movies/CinemaRecordsList/CinemaRecord/CinemaRecordDetails/CinemaRecordDetails.tsx
+++ front-end/StreamMates_FE/src/components/Movies/CinemaRecordsList/CinemaRecord/CinemaRecordDetails/CinemaRecordDetails.tsx
@@ -1,71 +1,101 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import style from "./CinemaRecordDetails.module.css";
 import { useLocation } from "react-router-dom";
-
 import { Series } from "../../../../../types/Series";
 import { Movie } from "../../../../../types/MovieType";
 import { constants } from "../../../../../constants/constants";
-
 import { Header } from "../../../Header/Header";
 import { Details } from "./Details/Details";
 import { CastSection } from "./CastSection/CastSection";
 import { PlayerSection } from "./PlayerSection/PlayerSection";
 import { ImageSection } from "./ImageSection/ImageSection";
 import { EpisodesSection } from "./EpisodesSection/EpisodesSection";
+import axios from "axios";
+import { Footer } from "../../../../Footer/Footer";
 
 export const CinemaRecordDetails = () => {
+    const BASE_URL = window.location.href.includes("local") ? "http://localhost:8080" : "https://streammate-org.onrender.com";
+
     const location = useLocation();
-    const [cinemaRecord] = useState<Movie | Series | undefined>(useLocation().state?.cinemaRecord);
+    const [cinemaRecord, setCinemaRecord] = useState<(Movie | Series) | null>(null);
     const [showPlayerSection, setShowPlayerSection] = useState<boolean>(!location.pathname.includes("/series/"));
     const [currentEpisodeURL, setCurrentEpisodeURL] = useState("");
+    const [loading, setLoading] = useState<boolean>(true); // State to track loading status
 
     // Refs:
     const playerRef = useRef<HTMLDivElement | null>(null);
 
+    useEffect(() => {
+        if (cinemaRecord) return;
+
+        const data = location.pathname.split("/");
+        const cinemaRecordId = data[data.length - 1];
+        const additionalUrl = location.pathname.includes("movie") ? "/get-movie-details" : "/get-series-details";
+
+        const getCinemaRecordDetails = async () => {
+            try {
+                const apiResponse = await axios.get(`${BASE_URL}${additionalUrl}`, {
+                    params: { id: cinemaRecordId },
+                    withCredentials: true
+                });
+
+                setCinemaRecord(apiResponse.data);
+                setLoading(false); // Set loading to false when data is fetched
+            } catch (error) {
+                console.log(error);
+                setLoading(false); // Ensure loading state is removed in case of error
+            }
+        };
+
+        getCinemaRecordDetails();
+    }, []);
 
     return (
-        <article className={style['cinema-record-details-container']}>
-            <Header setCinemaRecordsList={() => {}}/>
-            <Details cinemaRecord={cinemaRecord} />
-            <CastSection cinemaRecord={cinemaRecord} />
-            {cinemaRecord?.imagesList && <ImageSection imagesList={cinemaRecord?.imagesList} />}
-            {showPlayerSection && (
-                <>
-                    <div ref={playerRef}>
-                        <PlayerSection
-                            videoURL={currentEpisodeURL.length > 2 ? currentEpisodeURL : cinemaRecord?.videoURL}
+        <>
+            {loading ? (
+                <div className={style.loader}></div> // Показваме лоудъра, докато зареждат данни
+            ) : (
+                cinemaRecord && (
+                    <article className={style['cinema-record-details-container']}>
+                        <Header user={null} setCinemaRecordsList={() => { }} />
+                        <Details cinemaRecord={cinemaRecord} />
+                        <CastSection cinemaRecord={cinemaRecord} />
+                        {cinemaRecord?.imagesList && <ImageSection imagesList={cinemaRecord?.imagesList} />}
+                        {showPlayerSection && (
+                            <div ref={playerRef}>
+                                <PlayerSection
+                                    videoURL={currentEpisodeURL.length > 2 ? currentEpisodeURL : cinemaRecord?.videoURL}
+                                />
+                            </div>
+                        )}
+                        {location.pathname.split("/")[1] == "movies" && !showPlayerSection && (
+                            <div ref={playerRef}>
+                                <PlayerSection
+                                    videoURL={currentEpisodeURL.length > 2 ? currentEpisodeURL : cinemaRecord?.videoURL}
+                                />
+                            </div>
+                        )}
+
+                        {location.pathname.split("/")[1] == "series" && (
+                            <EpisodesSection
+                                playerRef={playerRef}
+                                setShowPlayerSection={setShowPlayerSection}
+                                setCurrentEpisodeURL={setCurrentEpisodeURL}
+                                allEpisodes={(cinemaRecord as Series).allEpisodes}
+                            />
+                        )}
+
+                        <img
+                            className={style['background-img']}
+                            src={constants.TMDB_IMG_URL + cinemaRecord?.backgroundImg_URL}
+                            alt={cinemaRecord?.title}
                         />
-                    </div>
-                </>
-            )}
-            {location.pathname.split("/")[1] == "movies" && !showPlayerSection && (
-                <>
-                    <div ref={playerRef}>
-                        <PlayerSection
-                            videoURL={currentEpisodeURL.length > 2 ? currentEpisodeURL : cinemaRecord?.videoURL}
-                        />
-                    </div>
-                </>
-            )}
+                        <span className={style['background-img-shadow']}></span>
 
-
-            {location.pathname.split("/")[1] == "series" && (
-                <>
-                    <EpisodesSection
-                        playerRef={playerRef}
-                        setShowPlayerSection={setShowPlayerSection}
-                        setCurrentEpisodeURL={setCurrentEpisodeURL}
-                        allEpisodes={(cinemaRecord as Series).allEpisodes}
-                    />
-                </>
+                        <Footer />
+                    </article>
+                )
             )}
-
-            <img
-                className={style['background-img']}
-                src={constants.TMDB_IMG_URL + cinemaRecord?.backgroundImg_URL}
-                alt={cinemaRecord?.title}
-            />
-            <span className={style['background-img-shadow']}></span>
-        </article>
+        </>
     );
 };
