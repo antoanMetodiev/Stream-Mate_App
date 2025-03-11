e91f3ef513b2b3961763f839d325c32fe9ee47f4
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-11 16:03:29 +0200

    add Pageable

--- back-end/src/main/java/bg/stream_mates/backend/feather/movies/repositories/MovieRepository.java
+++ back-end/src/main/java/bg/stream_mates/backend/feather/movies/repositories/MovieRepository.java
@@ -1,6 +1,8 @@
 package bg.stream_mates.backend.feather.movies.repositories;
 
 import bg.stream_mates.backend.feather.movies.models.entities.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,4 +19,10 @@ public interface MovieRepository extends JpaRepository<Movie, UUID> {
     @Query(value = "SELECT * FROM movies WHERE LOWER(title) LIKE LOWER(CONCAT('%', :movieName, '%'))" +
             " OR LOWER(search_tag) LIKE LOWER(CONCAT('%', :movieName, '%'))", nativeQuery = true)
     List<Movie> findByTitleOrSearchTagContainingIgnoreCase(@Param("movieName") String movieName);
+
+    Page<Movie> findAll(Pageable pageable);
+    long count();
+
+    @Query(value = "SELECT id, title, poster_img_url, release_date FROM movies", nativeQuery = true)
+    List<Object[]> getThirthyMoviesRawData(Pageable pageable);
 }
