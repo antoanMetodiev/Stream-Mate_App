7e0ccd25018e05ffeb3911eb349a2bf8a399e20f
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-11 15:55:25 +0200

    add aditional props

--- front-end/StreamMates_FE/src/types/CallNotification.ts
+++ front-end/StreamMates_FE/src/types/CallNotification.ts
@@ -7,4 +7,6 @@ export type CallNotification = {
     callType: CallType | string; // "video" или "audio"
     channelName: string; // WebRTC / Agora канал
     timestamp: string; // Време на обаждането (ISO формат)
+    callerNames: string,
+    callerImgUrl: string,
 };
