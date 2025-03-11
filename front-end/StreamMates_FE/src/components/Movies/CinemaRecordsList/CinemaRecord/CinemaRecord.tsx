3728f5cfa5fc7bc933eeb53d051542ffc8eb34b3
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-11 16:01:33 +0200

    add prop

--- front-end/StreamMates_FE/src/components/Movies/CinemaRecordsList/CinemaRecord/CinemaRecord.tsx
+++ front-end/StreamMates_FE/src/components/Movies/CinemaRecordsList/CinemaRecord/CinemaRecord.tsx
@@ -7,18 +7,21 @@ import style from "./CinemaRecord.module.css";
 interface CinemaRecordProps {
     cinemaRecord: (Movie | Series) | undefined;
     cinemaRecordsList: (Movie | Series)[] | undefined;
+    currentPaginationPage: number;
 }
 
 export const CinemaRecord = ({
     cinemaRecord,
-    cinemaRecordsList
+    cinemaRecordsList,
+    currentPaginationPage,
 }: CinemaRecordProps) => {
     const navigate = useNavigate();
 
     const navigateToDetails = (cinemaRecord: (Movie | Series) | undefined): void => {
         debugger;
         localStorage.setItem("LAST_CINEMA_RECORDS", JSON.stringify(cinemaRecordsList));
-        navigate(`${cinemaRecord?.id}`, { state: { cinemaRecord } });
+        localStorage.setItem("LAST_CURRENT_PAGE", JSON.stringify(currentPaginationPage));
+        navigate(`${cinemaRecord?.id}`);
     };
 
     return (
@@ -31,12 +34,15 @@ export const CinemaRecord = ({
                 src={constants.TMDB_IMG_URL + cinemaRecord?.posterImgURL}
                 alt={cinemaRecord?.title}
             />
-            <h2>
-                {cinemaRecord && cinemaRecord?.title.length > 20
-                    ? cinemaRecord?.title.substring(0, 20) + "..."
-                    : cinemaRecord?.title}
-            </h2>
-            <h4>{cinemaRecord?.releaseDate} • Movie</h4>
+
+            <div className={style['text-container']}>
+                <h2>
+                    {cinemaRecord && cinemaRecord?.title.length > 17
+                        ? cinemaRecord?.title.substring(0, 17) + "..."
+                        : cinemaRecord?.title}
+                </h2>
+                <h4>{cinemaRecord?.releaseDate} • Movie</h4>
+            </div>
         </div>
     );
 };
