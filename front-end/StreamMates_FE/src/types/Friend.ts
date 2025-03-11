54d17b829283f96f256cffc27f96c4d2d0ba8ed9
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-11 15:55:45 +0200

    add additional prop

--- front-end/StreamMates_FE/src/types/Friend.ts
+++ front-end/StreamMates_FE/src/types/Friend.ts
@@ -4,4 +4,5 @@ export type Friend = {
     firstName: string; // Съответства на firstName в Java
     lastName: string; // Съответства на lastName в Java
     profileImageURL?: string; // Опционално, съответства на nullable = true
+    realUserId: string;
 };
