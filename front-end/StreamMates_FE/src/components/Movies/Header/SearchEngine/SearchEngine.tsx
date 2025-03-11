71282709927a92ad70e87a3601afca88dd016292
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-11 15:58:49 +0200

    make search engine more flexible (work with movies and series))

--- front-end/StreamMates_FE/src/components/Movies/Header/SearchEngine/SearchEngine.tsx
+++ front-end/StreamMates_FE/src/components/Movies/Header/SearchEngine/SearchEngine.tsx
@@ -13,65 +13,68 @@ interface SearchEngineProps {
 export const SearchEngine = ({
     setCinemaRecordsList,
 }: SearchEngineProps) => {
+    const BASE_URL = window.location.href.includes("local") ? "http://localhost:8080" : "https://streammate-org.onrender.com";
     const location = useLocation();
     const navigate = useNavigate();
-
 
     async function searchForMovies(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         const inputElement = event.currentTarget.elements.namedItem("searchEngine") as HTMLInputElement;
-        const recordName = inputElement.value;
-        console.log(recordName);
-        const requestData: CinemaRecRequestDto = { recordName };
+        const title = inputElement.value;
 
-        const BASE_URL = window.location.href.includes("local") ? "http://localhost:8080" : "https://streammate-org.onrender.com";
-
+        debugger;
         try {
-            const apiResponse = await axios.post(BASE_URL + "/get-movies", requestData, {
-                withCredentials: true
+            const apiResponse = await axios.get(`${BASE_URL}/get-movies-by-title`, {
+                withCredentials: true,
+                params: { title }
             });
             const movies: Movie[] = apiResponse.data as Movie[];
             console.log(movies);
 
             setCinemaRecordsList(movies);
-            navigate("/movies/search/" + recordName);
+            navigate("/movies/search/" + title);
         } catch (error) {
             console.log(error);
         };
     };
 
     async function searchForSeries(event: React.FormEvent<HTMLFormElement>) {
+        debugger;
         event.preventDefault();
         const inputElement = event.currentTarget.elements.namedItem("searchEngine") as HTMLInputElement;
-        const recordName = inputElement.value;
-        console.log(recordName);
-        const requestData: CinemaRecRequestDto = { recordName };
-
-        const BASE_URL = window.location.href.includes("local") ? "http://localhost:8080" : "https://streammate-org.onrender.com";
+        const title = inputElement.value;
 
         try {
-            const apiResponse = await axios.post(BASE_URL + "/get-series", requestData, {
-                withCredentials: true
+            const apiResponse = await axios.get(`${BASE_URL}/get-series-by-title`, {
+                withCredentials: true,
+                params: { title }
             });
             const series: Series[] = apiResponse.data as Series[];
             console.log(series);
 
+            
             setCinemaRecordsList(series);
-            navigate("/series/search/" + recordName);
+            setTimeout(() => {
+                navigate("/series/search/" + title);
+            }, 200);
+            
         } catch (error) {
             console.log(error);
         };
     };
 
 
-
     return (
-        <form onSubmit={location.pathname.includes("/movies") ? searchForMovies : searchForSeries}>
-            <input
-                className={style['search-engine']}
-                name="searchEngine"
-                type="text"
-            />
-        </form>
+        <div className={style['search-container']}>
+            <form onSubmit={location.pathname.includes("/movies") ? searchForMovies : searchForSeries}>
+                <h3 className={style['search-h3']}>Search</h3>
+                <input
+                    className={style['search-engine']}
+                    name="searchEngine"
+                    type="text"
+                />
+            </form>
+        </div>
     );
+
 };
