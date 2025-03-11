b6ed4742a9d2250b6ecd3d84e3c4b18a4f5351b8
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-11 16:02:12 +0200

    enable transaction management

--- back-end/src/main/java/bg/stream_mates/backend/BackEndApplication.java
+++ back-end/src/main/java/bg/stream_mates/backend/BackEndApplication.java
@@ -2,8 +2,10 @@ package bg.stream_mates.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class BackEndApplication {
 
     public static void main(String[] args) {
