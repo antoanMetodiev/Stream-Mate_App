25354dc706101dda207b0cf0c7650dc070cb89cc
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-03-11 16:09:02 +0200

    add all/no args  constructor

--- back-end/src/main/java/bg/stream_mates/backend/feather/chat/models/dtos/ReceivedMessage.java
+++ back-end/src/main/java/bg/stream_mates/backend/feather/chat/models/dtos/ReceivedMessage.java
@@ -5,11 +5,15 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReceivedMessage {
 
     @NotBlank(message = "Message is empty!")
@@ -18,10 +22,17 @@ public class ReceivedMessage {
     @NotBlank(message = "Owner is empty!")
     private String owner;
 
+    @NotBlank(message = "Owner names cannot be empty!")
+    private String ownerNames;
+
+    private String ownerImgUrl;
+
     @NotBlank(message = "Receiver is empty!")
     private String receiver;
 
     @Enumerated(EnumType.STRING)
     @NotNull
     private MessageType messageType;
+
+    private String createdOn;
 }
